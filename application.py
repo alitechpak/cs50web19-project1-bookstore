7ccc202af009991cd2dae5d62014a61e0c5afa47
ali <alitechbeyond@gmail.com>
2020-03-28 08:19:04 +0500

    changed heroku uri

--- application.py
+++ application.py
@@ -20,7 +20,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine(os.getenv("DATABASE_URI"))
 db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
